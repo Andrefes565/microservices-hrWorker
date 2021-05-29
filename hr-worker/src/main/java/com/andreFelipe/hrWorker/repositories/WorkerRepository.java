89256f882600e22f7ab85e565aea47d858c6a768
Andrefes565 <andrexfes07@gmail.com>
2021-05-29 13:38:52 -0300

    implementação hr-worker

---
+++ hr-worker/src/main/java/com/andreFelipe/hrWorker/repositories/WorkerRepository.java
@@ -0,0 +1,10 @@
+package com.andreFelipe.hrWorker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.andreFelipe.hrWorker.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long>{
+	
+
+}
