5f3cd313ca1b58af3845fc9818160548f4fb701a
Andrefes565 <andrexfes07@gmail.com>
2021-05-29 13:48:57 -0300

    payroll criado

---
+++ hr-payroll/src/test/java/com/andreFelipe/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreFelipe.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
