1a16ba0a45427e9d0dc69dd6f0297cf50c3b44e4
Andrefes565 <andrexfes07@gmail.com>
2021-05-29 12:55:59 -0300

    projeto hr-worker criado

---
+++ hr-worker/src/test/java/com/andreFelipe/hrWorker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreFelipe.hrWorker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
