be0e8d56ef8610bc9ab37896f451327abec9de3e
Andrefes565 <andrexfes07@gmail.com>
2021-05-30 14:21:59 -0300

    servidor de configuração do worker

--- hr-worker/src/main/java/com/andreFelipe/hrWorker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/andreFelipe/hrWorker/resources/WorkerResource.java
@@ -23,12 +23,21 @@ public class WorkerResource {
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 	
+	@Value("${test.config}")
+	private String testConfig;
+	
 	@Autowired
 	private Environment env;
 
     @Autowired
     private WorkerRepository repository;
 
+    
+    @GetMapping(value ="/configs")
+    public ResponseEntity<Void> getConfigs() {
+    	logger.info("CONFIG = " + testConfig);
+    	return ResponseEntity.noContent().build();
+    }
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
